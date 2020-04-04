24d528d9c64a2dc14a9356deca82135c0fb1bd0a
Nurtugan Nuraly <nurtugan757@gmail.com>
2020-04-04 13:14:36 +0600

    Division action

--- WebApplication/Controllers/CalculatorController.cs
+++ WebApplication/Controllers/CalculatorController.cs
@@ -2,17 +2,35 @@ using Microsoft.AspNetCore.Mvc;
 
 namespace WebApplication
 {
+    [Route("[controller]/[action]")]
     public class CalculatorController : Controller
     {
+        
 
-        [Route("[controller]/{firstNumber:int}/{secondNumber:int}")]
+        [Route("{firstNumber:int}/{secondNumber:int}")]
         public IActionResult Sum(int firstNumber, int secondNumber)
         {
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '+';
             ViewData["firstNumber"] = firstNumber;
             ViewData["secondNumber"] = secondNumber;
             ViewData["result"] = firstNumber + secondNumber;
 
-            return View();
+            return View("Result");
         }
+
+        [Route("{firstNumber:int}/{secondNumber:int:min(1)}")]
+        public IActionResult Divide(int firstNumber, int secondNumber)
+        {
+            ViewData["action"] = RouteData.Values["action"].ToString();
+            ViewData["mark"] = '/';
+            ViewData["firstNumber"] = firstNumber;
+            ViewData["secondNumber"] = secondNumber;
+            ViewData["result"] = firstNumber / secondNumber;
+
+            return View("Result");
+        }
+
+
     }
 }
